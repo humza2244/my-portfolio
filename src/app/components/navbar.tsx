76e391800979ca644f91cfc47314d435e99b1389
humza2244 <hb.humza@gmail.com>
2025-04-19 01:07:41 -0400

    navbar + designs
    added a navbar created a new color scheme and started working on some design schemes

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    // Rainbow border wrapper
+    <div className="p-[2px] bg-gradient-to-r from-red-500 via-yellow-400 to-purple-500 rounded-bl-2xl rounded-br-2xl drop-shadow-2xl">
+      
+      {/* Actual navbar */}
+      <div className="grid grid-cols-3 items-center w-full h-16 px-3 bg-black text-white rounded-bl-2xl rounded-br-2xl">
+
+        {/* Left - Logo */}
+        <div className="flex items-center space-x-2">
+          <Image src="/hblogo.png" alt="hb logo" width={60} height={60} />
+          <div className="text-left font-mono font-bold text-lg">Humza Baig</div>
+        </div>
+
+        {/* Center - Nav Links */}
+        <div className="justify-self-center font-mono flex items-center space-x-10 h-full">
+          <a href="#about" className="hover:text-gray-400 transition duration-200">About Me</a>
+          <a href="#workexperience" className="hover:text-gray-400 transition duration-200">Work Experience</a>
+          <a href="#projects" className="hover:text-gray-400 transition duration-200">Projects</a>
+          <a href="#links" className="hover:text-gray-400 transition duration-200">Links</a>
+        </div>
+
+        {/* Right - Placeholder (with height for balance) */}
+        <div className="text-right h-full"></div>
+      </div>
+    </div>
+  );
+}
