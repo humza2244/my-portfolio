20a22bc08ec32f4b92c81556b72cfbad0378d184
humza2244 <hb.humza@gmail.com>
2025-04-19 15:44:00 -0400

    stars background update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from '@/components/Navbar'
+import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+export const metadata = {
+  title: 'Portfolio',
+  description: 'Humza Baig Portfolio',
+}
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Create Next App",
-  description: "Generated",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-fixed bg-no-repeat bg-gradient-to-b from-black via-[#31004a] to-[#12021a] bg-[length:100%_300%] bg-no-repeat text-white">
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
 'use client'
 import Navbar from './components/navbar'
 import { Typewriter } from 'react-simple-typewriter'
+import ParticlesComponent from './components/particles'
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="bg-white h-[5vh] w-[5vw]"></div>
-      <div className="relative w-full h-screen bg-black">
 
+      <div className="relative w-full h-screen">
 
-      {/* TYPEWRITE EFFECT*/}
-      <div className="relative w-full h-screen bg-black">
+        <div className="App">
+          <ParticlesComponent id="particles" />
+        </div>
+
         {/* TYPEWRITER CENTERED */}
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-mono z-50">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl font-mono z-50">
           <Typewriter
-            words={['Software Engineer', 'UX/UI Designer', 'Problem Solver', 'Meet: Humza Baig']}
+            words={[
+              'Software Engineer',
+              'UX/UI Designer',
+              'Problem Solver',
+              'Meet: Humza Baig',
+            ]}
             loop={false}
             cursor
             cursorStyle="|"
@@ -25,31 +32,20 @@ export default function Home() {
           />
         </div>
 
-      </div>
-        
-        {/* VERTICAL LINES LEFT */}
-
+        VERTICAL LINES LEFT
         <div className="absolute left-[5vw] top-[20vh] h-[50vh] w-[0.2vw] bg-gradient-to-b from-red-500 via-yellow-400 to-purple-500"></div>
         <div className="absolute left-[4.6vw] top-[20vh] h-[50vh] w-[0.2vw] bg-gradient-to-b from-red-500 via-yellow-400 to-purple-500"></div>
-
         <div className="absolute left-[5vw] top-[70vh] h-[14vh] w-[0.2vw] rotate-[320deg] origin-top-left bg-gradient-to-b from-purple-500 via-yellow-400 to-red-500"></div>
         <div className="absolute left-[4.6vw] top-[70vh] h-[14vh] w-[0.2vw] rotate-[320deg] origin-top-left bg-gradient-to-b from-purple-500 via-yellow-400 to-red-500"></div>
 
 
-        {/* VERTICAL LINES RIGHT */}
         <div className="absolute right-[5vw] top-[20vh] h-[50vh] w-[0.2vw] bg-gradient-to-b from-red-500 via-yellow-400 to-purple-500"></div>
         <div className="absolute right-[4.6vw] top-[20vh] h-[50vh] w-[0.2vw] bg-gradient-to-b from-red-500 via-yellow-400 to-purple-500"></div>
-
         <div className="absolute right-[5vw] top-[70vh] h-[14vh] w-[0.2vw] rotate-[45deg] origin-top-right bg-gradient-to-b from-purple-500 via-yellow-400 to-red-500"></div>
         <div className="absolute right-[4.6vw] top-[70vh] h-[14vh] w-[0.2vw] rotate-[45deg] origin-top-right bg-gradient-to-b from-purple-500 via-yellow-400 to-red-500"></div>
+       
 
       </div>
-      
-
-      
-      
-
-
     </>
-  ) 
+  )
 }
